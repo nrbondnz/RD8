b73b079c4203d8cebf156accab63bf31a0d729af
Bond in Auckland <nigel.bond@orcon.net.nz>
2021-11-25 00:20:23 +1300

    finished rolling learning

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Door : MonoBehaviour, IDoor
+{
+    [SerializeField] private float unlockingSpeed = 2;
+    [SerializeField] private float unlockingTime = 3;
+    [SerializeField] private bool isDoorUnlocked = false;
+
+    public void unlockDoor()
+    {
+        this.isDoorUnlocked = true;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDoorUnlocked)
+        {
+            unlockingTime -= Time.deltaTime;
+            transform.Translate(Vector3.down * Time.deltaTime * this.unlockingSpeed);
+            if (this.unlockingTime < 0)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WinningLevel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinningLevel : MonoBehaviour
+{
+    [SerializeField] private Material winningMaterial;
+
+    [SerializeField] private GameObject winningUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(WinningRoutine());
+        }
+    }
+
+    IEnumerator WinningRoutine()
+    {
+        GetComponent<MeshRenderer>().material = winningMaterial;
+        winningUI.SetActive(true);
+        Time.timeScale = 0.25f;
+        yield return new WaitForSeconds(1f);
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+}
