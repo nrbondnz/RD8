7984fb950b530bd4e7d6a58b0ee629026a8eb2f6
Bond in Auckland <nigel.bond@orcon.net.nz>
2023-02-25 15:21:12 +1300

    fixing scenes with new projection and sim ball

--- Assets/Scripts/Utilities/CameraFollow.cs
+++ Assets/Scripts/Utilities/CameraFollow.cs
@@ -16,7 +16,7 @@ using Utilities;
         public class CameraFollow : MonoBehaviour
         {
             private Transform _cameraTarget;
-            [SerializeField] private Vector3 offset;
+            [SerializeField] private Vector3 offset = new Vector3(-8f,-6f);
             private float _yOffset = 0.0f;
             [SerializeField] private float lowerBound = -5.0f;
             [SerializeField] private float uppedBound = 25.0f;
