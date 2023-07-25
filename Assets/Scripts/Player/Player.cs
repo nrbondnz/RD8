1068b403ee01883098b4ac76154f21cc73aa189b
Bond in Auckland <nigel.bond@orcon.net.nz>
2023-07-25 19:39:58 +1200

    working

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,44 +1,62 @@
 
 using System;
+using System.ComponentModel.Design.Serialization;
+using Unity.VisualScripting;
 
 namespace Player
 {
     public class Player
     {
+        private static Player Instance;
         private bool _isJumpButtonPressed = false;
         private bool _isGrounded = true;
         private bool _goingForwards = true;
         private float _hozInput;
         private float _vertInput;
 
+        public static Player getInstance()
+        {
+            if (Instance.IsUnityNull())
+            {
+                Instance = new Player();
+            }
+
+            return Instance;
+        }
+
+        public void Awake()
+        {
+            getInstance();
+        }
+        
         public float HozInput
         {
-            get => _hozInput;
-            set => _hozInput = value;
+            get => getInstance()._hozInput;
+            set => getInstance()._hozInput = value;
         }
 
         public float VertInput
         {
-            get => _vertInput;
-            set => _vertInput = value;
+            get => getInstance()._vertInput;
+            set => getInstance()._vertInput = value;
         }
 
         public bool IsJumpButtonPressed
         {
-            get => _isJumpButtonPressed;
-            set => _isJumpButtonPressed = value;
+            get => getInstance()._isJumpButtonPressed;
+            set => getInstance()._isJumpButtonPressed = value;
         }
 
         public bool IsGrounded
         {
-            get => _isGrounded;
-            set => _isGrounded = value;
+            get => getInstance()._isGrounded;
+            set => getInstance()._isGrounded = value;
         }
 
         public bool GoingForwards
         {
-            get => _goingForwards;
-            set => _goingForwards = value;
+            get => getInstance()._goingForwards;
+            set => getInstance()._goingForwards = value;
         }
 
         public override string ToString()
--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -1,20 +1,36 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Utilities;
 
 namespace Player
 {
-    public class PlayerInputController : MonoBehaviour
-    {
+    public class PlayerInputController : MonoBehaviour {
+    
+        private Player player;
+        public void Start()
+        {
+            player = Player.getInstance();
+        }
 
         public void OnJump()
         {
             Debug.Log("Jump Pressed");
+            player.IsJumpButtonPressed = true;
+            Actions.OnPlayerChanged(player);
         }
 
         public void OnMovement(InputValue pInputValue)
         {
             Debug.Log("Movement : " + pInputValue);
+            Vector2 input2D= pInputValue.Get<Vector2>();
+            player.HozInput = input2D.x;
+            player.VertInput = input2D.y;
+            UnityEngine.Debug.Log(input2D);
+            Actions.OnPlayerChanged(player);
+            //GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+            //gameObjects[0].GetComponent<Rigidbody>()
+            //    .AddForce(new Vector3(player.HozInput, 0.0f, player.VertInput), ForceMode.Impulse);
         }
-        
     }
 }
